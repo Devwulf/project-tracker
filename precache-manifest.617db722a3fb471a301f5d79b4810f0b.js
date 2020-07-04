8c48c102aa91d6372cfecebd886ce96c6d2775cd
Mark Malabanan <mark.malabanan24@gmail.com>
2020-07-04 13:45:13 -0400

    Updates

---
+++ precache-manifest.617db722a3fb471a301f5d79b4810f0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01c76a615e4c4b53cd92acb755ef990b",
+    "url": "/project-tracker/index.html"
+  },
+  {
+    "revision": "d1d28656c6858eba8934",
+    "url": "/project-tracker/static/css/main.65817cd4.chunk.css"
+  },
+  {
+    "revision": "cc61f9d05ef3e6207e69",
+    "url": "/project-tracker/static/js/2.b6768fe8.chunk.js"
+  },
+  {
+    "revision": "41c34e7530cd0d18db13c31dac341321",
+    "url": "/project-tracker/static/js/2.b6768fe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1d28656c6858eba8934",
+    "url": "/project-tracker/static/js/main.fffa74f3.chunk.js"
+  },
+  {
+    "revision": "d40ff38f27fe7d5e7ff6",
+    "url": "/project-tracker/static/js/runtime-main.7b8b7c42.js"
+  }
+]);
